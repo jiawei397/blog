509c13438e7d477a947495a857fcbb63831d0825
jiawei397 <jw397@126.com>
2017-06-28 11:04:32 +0800

    注释

--- middlewares/check.js
+++ middlewares/check.js
@@ -2,19 +2,29 @@
  * Created by Administrator on 2017/1/6 0006.
  */
 module.exports = {
-    checkLogin: function checkLogin(req, res, next) {
-        if (!req.session.user) {
-            req.flash('error', '未登录');
-            return res.redirect('/signin');
-        }
-        next();
-    },
-
-    checkNotLogin: function checkNotLogin(req, res, next) {
-        if (req.session.user) {
-            req.flash('error', '已登录');
-            return res.redirect('back');//返回之前的页面
-        }
-        next();
+  /**
+   * 检查是否登陆，未登陆的话，跳转到登陆页
+   * @author jw
+   * @date 2017-06-28
+   */
+  checkLogin: function checkLogin(req, res, next) {
+    if (!req.session.user) {
+      req.flash('error', '未登录');
+      return res.redirect('/signin');
     }
+    next();
+  },
+
+  /**
+   * 检查是否登陆，登陆的话，跳转到上一个页面
+   * @author jw
+   * @date 2017-06-28
+   */
+  checkNotLogin: function checkNotLogin(req, res, next) {
+    if (req.session.user) {
+      req.flash('error', '已登录');
+      return res.redirect('back');//返回之前的页面
+    }
+    next();
+  }
 };
